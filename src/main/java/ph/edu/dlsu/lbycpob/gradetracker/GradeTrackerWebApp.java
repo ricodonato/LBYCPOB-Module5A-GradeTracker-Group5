290463761e4470f246da38e1b34c4708e3f20530
drew <drew_nathaniell_cobreros@dlsu.edu.ph>
2026-07-16 17:51:41 +0800

    fix: correct GradeTrackerWebApp class reference in main method

--- src/main/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradeTrackerWebApp {
-    static void main(String[] args) {
-        SpringApplication.run(GradeTrackerSpringBootApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(GradeTrackerWebApp.class, args);
     }
 }
